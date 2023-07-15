3671d6fed3b22333a06a53ff3f46634a8fe59ef9
Assou <assia.asri@gmail.com>
2023-07-15 19:43:29 +0200

    création du modèle pour la table Users

---
+++ server/models/Users.js
@@ -0,0 +1,22 @@
+module.exports = (sequelize, DataTypes) => {
+    const Users = sequelize.define("Users", {
+        userId: {
+            type: DataTypes.INTEGR,
+            allowNull: false
+        },
+        userName: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        userEmail: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        userPassword: {
+            type: DataTypes.STRING,
+            allowNull: false
+        }
+    })
+
+    return Users
+}
