6aaffd1daa477595acc71af1414daf2097e55bd4
Assou <assia.asri@gmail.com>
2023-07-15 19:46:55 +0200

    creation de la variable db dans le fichier index.js pour la synchronisation des models de table

--- server/index.js
+++ server/index.js
@@ -1,7 +1,10 @@
 //build node js and express server
 const express = require("express");
 const app = express();
+const db = require('./models')
 
-app.listen(3001, () => {
-    console.log("Server running on port 3001");
+db.sequelize.sync().then( () => {
+    app.listen(3001, () => {
+        console.log("Server running on port 3001");
+    })
 })
