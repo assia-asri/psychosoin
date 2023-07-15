57f38b9edd719fd65b9ebd146114e04a2d05a9ec
Assou <assia.asri@gmail.com>
2023-07-15 19:30:26 +0200

    changement du port de server pour 3001

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 //build node js and express server
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 
-app.listen(3002, () => {
-    console.log('Server running on port 3002');
+app.listen(3001, () => {
+    console.log("Server running on port 3001");
 })
